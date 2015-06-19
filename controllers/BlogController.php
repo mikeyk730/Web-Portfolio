30c6c36a454923a09e18d27d9805e0694eeecb7f
unknown <mikeyk730@gmail.com>
2015-06-19 12:53:53 -0400

    unpublished posts return a 404 if not the owner.  remove crud actions from blog

--- controllers/BlogController.php
+++ controllers/BlogController.php
@@ -14,33 +14,6 @@ use yii\filters\VerbFilter;
  */
 class BlogController extends Controller
 {
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * Lists all Post models.
-     * @return mixed
-     */
-    public function actionList()
-    {
-        $searchModel = new PostSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
     /**
      * Displays a single Post model.
      * @param integer $id
@@ -48,11 +21,18 @@ class BlogController extends Controller
      */
     public function actionView($id=null, $url_text=null)
     {
+        $model = $this->findModel($id, $url_text);
+        if (!$model->is_published){
+            if (!\Yii::$app->user->can('modifyAlbum', ['content' => $model])) {
+                throw new NotFoundHttpException("The requested page does not exist."); 
+            }
+        }
+
         $is_mobile = \Yii::$app->devicedetect->isMobile() && !\Yii::$app->devicedetect->isTablet();;
 
         $this->layout = $is_mobile ? 'mobile' : 'posts';
         return $this->render('view', [
-            'model' => $this->findModel($id, $url_text),
+            'model' => $model,
         ]);
     }
 
@@ -65,56 +45,6 @@ class BlogController extends Controller
     }
 
     /**
-     * Creates a new Post model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Post();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing Post model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Post model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
      * Finds the Post model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
@@ -124,7 +54,7 @@ class BlogController extends Controller
     protected function findModel($id, $url_text=null)
     {
         $model = ($url_text === null) ? 
-                 Post::findOne($id) : Post::findOne(['url_text' => $url_text]);
+            Post::findOne($id) : Post::findOne(['url_text' => $url_text]);
         
         if ($model === null)
             throw new NotFoundHttpException('The requested page does not exist.');
